1e4a258ef790f15a3bfb1f4a7cf45b9bd92ade1c
ankang <ankangz98@gmail.com>
2025-09-07 10:51:36 +0800

    feat(FlowGram): 添加默认数据并更新编辑器初始化逻辑

--- src/pages/FlowGram/constants.ts
+++ src/pages/FlowGram/constants.ts
@@ -1,5 +1,73 @@
 import { FlowDocumentJSON } from './typings';
 
+export const defaultData: FlowDocumentJSON = {
+  nodes: [
+    {
+      id: 'start_0',
+      type: 'start',
+      meta: {
+        position: {
+          x: 180,
+          y: 573.7,
+        },
+      },
+      data: {
+        title: 'Start',
+        outputs: {
+          type: 'object',
+          properties: {
+            query: {
+              type: 'string',
+              default: 'Hello Flow.',
+            },
+            enable: {
+              type: 'boolean',
+              default: true,
+            },
+            array_obj: {
+              type: 'array',
+              items: {
+                type: 'object',
+                properties: {
+                  int: {
+                    type: 'number',
+                  },
+                  str: {
+                    type: 'string',
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    {
+      id: 'end_0',
+      type: 'end',
+      meta: {
+        position: {
+          x: 3008,
+          y: 573.7,
+        },
+      },
+      data: {
+        title: 'End',
+        inputsValues: {
+          success: { type: 'constant', content: true, schema: { type: 'boolean' } },
+          query: { type: 'ref', content: ['start_0', 'query'] },
+        },
+      },
+    },
+  ],
+  edges: [
+    {
+      sourceNodeID: 'start_0',
+      targetNodeID: 'end_0',
+    },
+  ],
+};
+
 export const initialData: FlowDocumentJSON = {
   nodes: [
     {
--- src/pages/FlowGram/editor.tsx
+++ src/pages/FlowGram/editor.tsx
@@ -4,26 +4,39 @@ import '@flowgram.ai/free-layout-editor/index.css'; // 加载样式
 import './editor.less';
 
 // import { NodeList } from './components/node-list';
-import { initialData } from './constants';
+import { defaultData, initialData } from './constants';
 import { nodeRegistries } from './nodes';
 import { useEditorProps } from './hooks' // 画布详细的 props 配置
 import { DemoTools } from './components/tools';
 import { SidebarProvider, SidebarRenderer } from './components/sidebar';
+// import { debounce } from 'lodash-es'
 
 const Editor: React.FC = () => {
-  const editorProps = useEditorProps({nodes:[], edges:[]}, nodeRegistries);
+  const editorProps = useEditorProps(defaultData, nodeRegistries);
   const ref = useRef<FreeLayoutPluginContext | undefined>();
 
+  // 加载数据
   useEffect(() => {
     setTimeout(() => {
       // 初始化数据
-      ref.current?.document.fromJSON(initialData)
+      // ref.current?.document.fromJSON(initialData)
+      ref.current?.document.reload(initialData)
     }, 1000)
     setTimeout(() => {
       // 加载后触发画布的 fitview 让节点自动居中
       ref.current?.document.fitView()
     }, 1100)
   }, [])
+
+  // useEffect(() => {
+  //   // 监听画布变化 延迟 1 秒 保存数据, 避免画布频繁更新
+  //   const toDispose = ref.current?.document.onContentChange(debounce(() => {
+  //       // 通过 toJSON 获取画布最新的数据
+  //       const data = ref.current?.document.toJSON();
+  //       console.log(data);
+  //   }, 1000))
+  //   return () => toDispose?.dispose()
+  // }, [])
 
   return (
     <FreeLayoutEditorProvider ref={ref as React.RefObject<FreeLayoutPluginContext>} {...editorProps}>
